63c5b59d44c151a92a7b4087b9b390340b8509bf
Divya-208 <divyagulati61@gmail.com>
2018-06-15 01:48:10 -0700

    add class assignments

--- assg.13-6/assgn3a.py
+++ assg.13-6/assgn3a.py
@@ -1,4 +1,4 @@
-# q;1 create the list with user defined input
+#3;1 create the list with user defined input
 print("enter the list of items")
 list=[ ]
 x=input("1")
@@ -11,7 +11,7 @@ print("\n")
 
 
 #Q2 add the following list to the upper created list
-['google','apple','facebook','microsoft','tesla']
+# ['google','apple','facebook','microsoft','tesla']
 L2=['google','apple','facebook','microsoft','tesla']
 print(L2)
 print(list.extend(L2))
@@ -19,15 +19,15 @@ print(list)
 print("\n")
 
 #Q3 count the no.of times object occur
-print(list.count(1))
+ print(list.count(1))
 
-#Q4 create the list with no. and sort it in ascending order
+Q4 create the list with no. and sort it in ascending order
 list=[1,3,2,6,5]
 print(list)
 print(list.sort( ))
 print(list)
 
- #Q5 enter two array nd merge them nd save them, in another variable
+ Q5 enter two array nd merge them nd save them, in another variable
 A=[1,2,3]
 print(A)
 B=[ 6,4,5]
@@ -39,7 +39,39 @@ print(B)
 C=(A,B)
 print(C)
 C=A+B
-print(C)
+print(C
 
 
+#Q:6 stack nd queue
+print("stack")
+l1=["my","new","list"]
+print(l1)
+l1.append("of")
+print(l1)
+l1.append("python")
+print(l1)
+print(l1.pop())
+print(l1)
+print(l1.pop())
+print(l1)
+print("\n")
+print("queue")
+l2=["easy","is","python",]
+print(l2)
+l2.reverse()
+print(l2)
+print(l2.pop())
+print(l2)
 
+#Q:7 count the even and odd no.
+l3=[1,2,3,4]
+print(l3)
+even=0:
+odd=0:
+for no. in l3:
+	if(number%2==1):
+		odd=odd+1
+	if(number%2==0):
+		even=even+1
+print("number of evenis:",even)
+print("number of odd is :,odd")
